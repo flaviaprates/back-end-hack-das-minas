ad14c66fa9d0c2f31865fa121aac2e1c63b6ca44
flaviaprates <flaviadeaprates@gmail.com>
2022-07-02 00:38:26 -0300

    Teste no deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,15 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
+        // host: process.env.DB_HOST,
+        host: "ec2-54-165-178-178.compute-1.amazonaws.com",
         port: 5432,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        // user: process.env.DB_USER,
+        user: "qzynksgchmpthq",
+        // password: process.env.DB_PASSWORD,
+        password: "1400068f4ff42f4b868af2b8d43925552679faaf52a117c55029981b75731ca1",
+        // database: process.env.DB_DATABASE
+        database: "d85q10umlu7bcn"
     },
     ssl: {
         rejectUnauthorized: false
