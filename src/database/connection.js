91a4fc76d698f465c75dbdc6410f22b2ec5c74e4
flaviaprates <flaviadeaprates@gmail.com>
2022-07-01 22:17:42 -0300

    Conexão com a Heroku

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
+        host: "ec2-54-165-178-178.compute-1.amazonaws.com",
         port: 5432,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        user: "qzynksgchmpthq",
+        password: "1400068f4ff42f4b868af2b8d43925552679faaf52a117c55029981b75731ca1",
+        database: "d85q10umlu7bcn"
     },
     ssl: {
         rejectUnauthorized: false,
